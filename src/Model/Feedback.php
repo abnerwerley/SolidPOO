11377df7a3971ac10804ca57a92874078e480bc9
abnerwerley <abnerwerley77@gmail.com>
2022-02-21 18:28:43 -0300

    @abnerwerley[Task: continuando extração]

--- src/Model/Feedback.php
+++ src/Model/Feedback.php
@@ -22,4 +22,14 @@ class Feedback
         $this->depoimento = $depoimento;
 
     }
+
+    public function recuperarNota(): int
+    {
+        return $this->nota;
+    }
+
+    public function recuperarDepoimento(): ?string
+    {
+        return $this->Depoimento;
+    }
 }
